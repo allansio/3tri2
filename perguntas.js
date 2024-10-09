355c4fdd0f996a5be00a4c83cadb236ac80de0db
allansio <142339294+allansio@users.noreply.github.com>
2024-10-09 11:35:55 -0300

    b

---
+++ perguntas.js
@@ -0,0 +1,100 @@
+criaCartao(
+    'História',
+    'Quem foi o ditador do Brasil em 1944?',
+    'Getúlio Vargas')
+    
+    criaCartao(
+    'História',
+    'Quando foi a segunda guerra mundial?  ',
+    '1939-1945')
+    
+    criaCartao(
+    'História',
+    'Quando foi a primeira guerra mundial?',
+    '1914-1918')
+    
+    criaCartao(
+    'História',
+    'Quem era o ditador da Alemanha em 1939?',
+    'Adolf Hitler')
+    
+    criaCartao(
+    'História',
+    'Quem era o ditador da Itália em 1939? ',
+    'Benito Mussolini')
+    
+    criaCartao(
+    'História',
+    'Quem era o ditador da União Soviética em 1939?',
+    'Stalin')
+    
+    criaCartao(
+    'História',
+    'Quem era o ditador da Espanha em 1939?',
+    'General Franco')
+    
+    criaCartao(
+    'História',
+    'Quem era o ditador de Portugal em 1939?',
+    'Salazar')
+    
+    criaCartao(
+    'História',
+    'Qual país fez a primeira bomba atômica?',
+    'Estados Unidos')
+    
+    criaCartao(
+    'História',
+    'Qual país recebeu a primeira bomba atômica?',
+    'Japão')
+
+    criaCartao(
+    'Geografia',
+    'Qual é a capital do Canadá?',
+    'Ottawa')
+    
+    criaCartao(
+    'Geografia',
+    'Qual a capital de Antígua e Barbuda?',
+    'Saint John´s')
+    
+    criaCartao(
+    'Geografia',
+    'Qual a capital de Nicarágua? ',
+    'Manágua')
+    
+    criaCartao(
+    'Geografia',
+    'Qual a capital de Burkina Faso?',
+    'Uagadugu')
+    
+    criaCartao(
+    'Geografia',
+    'Qual a capital do Butão?',
+    'Timbu')
+    
+    criaCartao(
+    'Geografia',
+    'Qual é o país mais populoso do mundo?',
+    'Índia')
+    
+    criaCartao(
+    'Geografia',
+    'Qual é o país menos populoso do mundo?',
+    'Vaticano')
+    
+    criaCartao(
+    'Geografia',
+    'Qual é o o país onde ocorreu a Primeira Revolução industrial? ',
+    'Inglaterra')
+    
+    criaCartao(
+    'Geografia',
+    'Em qual país ocorreu a Revolta da Vacina?',
+    'Brasil')
+    
+    criaCartao(
+    'Geografia',
+    'Em qual pais fica o Vale do Silício?',
+    'Estados Unidos')
+    
