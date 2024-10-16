be59fc5bc0fdf10aa06066cd6bd04b57d61fe7fd
allansio <142339294+allansio@users.noreply.github.com>
2024-10-16 12:13:27 -0300

    g

--- main.js
+++ main.js
@@ -11,6 +11,15 @@ function criaCartao(categoria, pergunta, resposta){
     <div class="resposta-cartao">
       <p>${resposta}</p>
 </div>`;
+   let respostaEstaVisivel = false;
+   function viraCartao(){
+    respostaEstaVisivel = !respostaEstaVisivel;
+    cartao.classList.toggle('active',respostaEstaVisivel);
+   }
+   cartao.addEventListener('click',viraCartao);
+   
+
+
 container.appendChild(cartao);
 
 }
